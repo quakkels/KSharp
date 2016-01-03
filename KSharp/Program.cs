463a7e3e23f18c6f26ad0c8648cf6bbfc5148307
quakkels <quakkels@gmail.com>
2016-01-03 09:39:34 -0600

    Added parser driver code to Main()

--- KSharp/Program.cs
+++ KSharp/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace KSharp
+﻿using KSharp.Parser;
+using System.Collections.Generic;
+
+namespace KSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var parser = new Parsr(new Dictionary<char, int> {
+                { '<', 10 },
+                { '+', 20 },
+                { '-', 30 },
+                { '*', 40 }
+            });
+
+            var driver = new Driver(parser);
+
+            driver.MainLoop();
         }
     }
 }
